8c311e51510b5322abad8f107ef0926efb243267
zwim <martin.zwicknagl@kirchbichl.net>
2022-08-18 10:11:58 +0200

    Make a bit more robust; improve example a bit

--- suntime.lua
+++ suntime.lua
@@ -557,8 +557,10 @@ end
         if not self.midnight then
             self.midnight = self.noon + 12
         end
-        if not self.midnight_beginning then
+        if not self.midnight_beginning and self.midnight then
             self.midnight_beginning = self.midnight - 24
+        elseif not self.midnight and not self.midnight_beginning then
+            self.midnight = self.midnight_beginning + 24
         end
     elseif self.rise and not self.set then -- only sunrise on that day
         self.midnight = nil
